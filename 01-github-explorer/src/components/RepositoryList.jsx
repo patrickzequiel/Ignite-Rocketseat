240afd5868be970049eb0f498e42ff8a6a4c5a22
Patrick Cruz <patrickezequielscruz@gmail.com>
2021-04-02 16:43:20 -0300

    Propriedades no React

---
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,19 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+    name: 'unform',
+    descrption: 'Forms in React',
+    link: 'https://gihub.com/unform/unform'
+}
+
+export function RepositoryList() {
+    return (
+        <section className='repository-List'>
+            <h1>Lista de repositório</h1>
+
+            <ul>
+                <RepositoryItem repository={repository} />
+            </ul>
+        </section>
+    )
+}
