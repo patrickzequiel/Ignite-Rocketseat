6bee5aec9f8e446974cab78d1db9947172d9d48a
Patrick Cruz <patrickezequielscruz@gmail.com>
2021-04-03 22:03:53 -0300

    Estrutura da Aplicação

---
+++ 02-dtmoney/src/styles/global.ts
@@ -0,0 +1,58 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+
+    :root {
+        --background: #f0f2f5
+        --red: #E52E4D;
+        --blue: #5429CC;
+
+        --blue-light: #36933FF;
+
+        --text-litle: #463F5F;
+        --text-boby: #969CB3;
+
+        --background: #F0F2F5;
+        --shape: #FFFFFF;
+}
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        background: var(--background);
+        -webkit-font-smooting: antialiased;
+    }
+
+    html{
+        @media (max-width: 1000px) {
+            font-size: 93.75%;
+        }
+
+        @media (max-width: 720px) {
+            font-size: 87.5%;
+        }
+    }
+
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, strong {
+        font-weight: 600;
+    }
+        
+    button{
+        cursor: pointer;
+    }
+
+
+    [disabled] {
+        opacity: 0.6px;
+        cursor: not-allowed;
+    }
+`
